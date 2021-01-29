0f3d7a5b8b28e1dca83f85a0a035e4567d8b5b00
Fernando Ferreira Bueno <fernandoferreirabueno@MacBook-Air-de-Fernando.local>
2021-01-29 18:07:00 -0300

    Headers HTTP e Ajuste de Imagem

--- app.js
+++ app.js
@@ -10,9 +10,18 @@ var artist_routes = require('./routes/artist');
 var album_routes = require('./routes/album');
 var song_routes = require('./routes/song');
 
-//Rotas
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+//Header HTTP
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
 
 //Rotas do Backend (API)
 app.use('/api', user_routes);
